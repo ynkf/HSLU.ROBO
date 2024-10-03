323ef67854442ba08fb9da10e9f197f2d8ec4f73
ynkf <dev.ynkf01@gmail.com>
2024-10-03 12:03:35 +0000

    init

---
+++ wheel_command_publisher.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import rospy
+from duckietown_msgs.msg import WheelsCmdStamped
+from std_msgs.msg import Header
+
+
+
+class WheelCommandPublisher:
+
+    def __init__(self, robot_name): 
+        # initialize a node with a name, annonymous=True ensures that the name is unique
+        rospy.init_node('wheel_command_publisher', anonymous=True)
+
+        # create a publisher for a topic of type duckietown_msgs.msg.WheelsCmdStamped
+        topic = '/' + robot_name + '/wheels_driver_node/wheels_cmd'
+        self.publisher = rospy.Publisher(topic, WheelsCmdStamped, queue_size = 10)
+        rospy.sleep(2.0)
+        
+        # create a WheelsCmdStamped message
+        self.command = WheelsCmdStamped()
+        self.command.header = Header()
+   
+
+    def turn_wheels(self, velocity_left, velocity_right):
+        self.command.vel_left = velocity_left
+        self.command.vel_right = velocity_right
+
+        self.command.header.stamp = rospy.Time.now()
+        self.publisher.publish(self.command)
+        rospy.loginfo("Publishing wheel command")
+        
+
+    def run(self):
+        self.turn_wheels(0.5, -0.5)
+        rospy.sleep(5) # wait 3 seconds
+        # stop wheels
+        self.turn_wheels(0.0, 0.0)
+
+if __name__ == '__main__':
+    wcp = WheelCommandPublisher("alpha")
+    wcp.run()
