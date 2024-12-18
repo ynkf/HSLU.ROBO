00f7fb7cb8ee080c41103d027b43c4a8280336a4
Yannick Frischknecht <yannick.frischknecht@leuchterag.ch>
2024-12-18 11:31:43 +0100

    adds config.py as base to qt-bot

---
+++ qt-bot/src/QTrobot_dev/qt_robot/src/config.py
@@ -0,0 +1,3 @@
+#place this file in the same folder as the GeminiAdapter class
+raise NotImplementedError("Please fill in the API key and remove this line, But do not commit the API key to the repository!!")
+api_key = ""
